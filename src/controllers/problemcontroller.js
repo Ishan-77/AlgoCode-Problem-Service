7ddf3103c10b70c722106c63ead3a62b4c7c2727
Ishan Purohit <ishan@Ishans-MacBook-Air.local>
2026-02-24 01:53:35 +0530

    Added routing structure for versioned API for Problems

---
+++ src/controllers/problemcontroller.js
@@ -0,0 +1,37 @@
+
+function pingCheckProblemController(req,res) {
+    return res.json({message:"Problem Controller is up,Routes are correct"});
+}
+
+
+function addProblem(req,res) {
+
+}
+
+function getProblem(req,res) {
+
+}
+
+function getProblems(req,res) {
+
+}
+
+function deleteProblem(req,res) {
+
+}
+
+function updateProblem(req,res) {
+
+}
+
+
+module.exports = {
+
+    addProblem,
+    getProblem,
+    getProblems,
+    deleteProblem,
+    updateProblem,
+    pingCheckProblemController
+
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 
-const {PORT} = require('./config/server.config')
+const {PORT} = require('./config/server.config');
+const apiRouter = require('./routes');
 
 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
+
+
+app.use('/api',apiRouter);
 
 
 app.get('/ping',(req,res)=> {
